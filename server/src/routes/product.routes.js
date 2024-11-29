cc3ef0022c0d37878f255bbb92f8afcd163704b5
MiguelDev725 <miguel.acevedo.ttc.725@unilibre.edu.co>
2024-11-29 14:51:21 -0500

    post en el getProducts

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -4,6 +4,7 @@ import { getProducts, createProduct } from "../controllers/product.controller.js
 const router = Router();
 
 router.get("/products", getProducts);
+router.post("/products", getProducts);
 router.post("/newproduct", createProduct);
 
 export default router;
